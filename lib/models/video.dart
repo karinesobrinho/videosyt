100494b337fde8c158f514f75bf15e071c39b098
karine sobrinho <karine.silva.sobrinho@gmail.com>
2020-11-03 16:42:14 -0300

    recebendo lista de videos + api

---
+++ lib/models/video.dart
@@ -0,0 +1,17 @@
+class Video {
+  final String id;
+  final String title;
+  final String channel;
+  final String thumb;
+
+  Video({this.id, this.title, this.channel, this.thumb});
+
+  factory Video.fromJson(Map<String, dynamic> json) {
+    return Video(
+      id: json['id']['videoId'],
+      title: json['snippet']['title'],
+      channel: json['snippet']['channelTitle'],
+      thumb: json['snippet']['thumbnails']['high']['url'],
+    );
+  }
+}
