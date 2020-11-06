9e82a775d1706eaea376b9bc932580b2fa42c49a
karine sobrinho <karine.silva.sobrinho@gmail.com>
2020-11-05 23:59:05 -0300

    bloc 1

---
+++ lib/bloc/videos_bloc.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:videosyt/models/video.dart';
+import '../api.dart';
+
+class VideosBloc implements BlocBase {
+  Api api;
+
+  List<Video> videos;
+
+  final StreamController _videosController = StreamController();
+  Stream get outVideos => _videosController.stream;
+
+  final StreamController _searchController = StreamController();
+  Sink get inSearch => _searchController.sink;
+
+  VideosBloc() {
+    api = Api();
+
+    _searchController.stream.listen(_search);
+  }
+
+  void _search(String search) async {
+    videos = await api.search(search);
+  }
+
+  @override
+  void dispose() {
+    _videosController.close();
+    _searchController.close();
+  }
+}
